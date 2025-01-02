b65405b925ff0f0c4fdfa7a1ce56ebb2e4c37441
dikshantkanela <dikshant7156@gmail.com>
2025-01-02 13:21:23 +0530

    Error Layout for user

--- app.js
+++ app.js
@@ -51,43 +51,55 @@ app.get("/campgrounds/new", (req, res) => {
   res.render("campgrounds/new.ejs"); //to create a new campground
 });
 
-app.post("/campgrounds", async (req, res,next) => {
- 
+app.post("/campgrounds", catchAsync(async (req, res,next) => { 
+  if(!req.body.campground){
+    throw new ExpressError("Invalid Campground Data",400); // if we try to do cleverness with postman 
+  }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
- 
-});
 
-app.get("/campgrounds/:id", async (req, res) => {
+}));
+
+app.get("/campgrounds/:id", catchAsync(async (req, res) => {
   //route to show detail of a specfic campgorund (ID)
   const { id } = req.params;
   const campground = await Campground.findById(id);
   res.render("campgrounds/show.ejs", { campground });
-});
+}));
 
-app.get("/campgrounds/:id/edit", async (req, res) => {  
+app.get("/campgrounds/:id/edit", catchAsync(async (req, res) => {  
   const { id } = req.params;  //edit wale form me tujhe ek specific camp ka data bhejna hai
   const campground = await Campground.findById(id);
   res.render("campgrounds/edit.ejs", { campground });  //essentially first step is to got to a edit form!
-});
+}));
 
-app.put("/campgrounds/:id", async (req, res) => { // form sends this PUT request
+app.put("/campgrounds/:id", catchAsync(async (req, res) => { // form sends this PUT request
   const { id } = req.params;
   const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }, { new: true });
   // Redirect to the show page of the updated campground
   res.redirect(`/campgrounds/${campground._id}`);
-});
+}));
 
-app.use((err,req,res,next)=>{
-  res.send("OH boy something went wrong!")
-})
-
-app.delete("/campgrounds/:id",async (req,res)=>{
+app.delete("/campgrounds/:id",catchAsync(async (req,res)=>{
   const {id} = req.params;
   await Campground.findByIdAndDelete(id);
   res.redirect("/campgrounds")
-});
+}));
+
+app.all("*",(req,res,next)=>{
+  next(new ExpressError("Page not found",404));
+})
+
+app.use((err,req,res,next)=>{
+  const {statusCode = 500} = err;
+  if(!err.message){
+    err.message ="Something went wrong!";
+  }
+  res.status(statusCode).render('error.ejs',{err})
+})
+
+
 // app.get("/newcamp", async(req,res)=>{
 //     const camp = new Campground({title:"Ramada",description:"Situated in Kasuali"});
 //     await camp.save();
--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,5 +1,5 @@
 const catchAsync =  (fn)=> (req,res,next)=>{
-    return fn(req,res,next).catch(next((e)=>e));
+    return fn(req,res,next).catch(next);
 }
 
 module.exports = catchAsync;
